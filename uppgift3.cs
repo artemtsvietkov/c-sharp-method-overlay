76b41c0a7a7def6168116caa165202dbe0bf932b
アルテム <143085970+artemtsvietkov@users.noreply.github.com>
2025-05-28 00:10:57 +0200

    Update uppgift3.cs

--- uppgift3.cs
+++ uppgift3.cs
@@ -1,20 +1,40 @@
+using System;
+
 class Program
 {
     static void Main()
-    {                
-        Console.WriteLine(Add(1, 2, 4));              
-        Console.WriteLine(Add(0.3, 0.2, 0.24534));      
-        Console.WriteLine(Add("text", "txet"));     
-        Console.WriteLine(Add('A', 'D', 'D'));        
+    {
+        Console.WriteLine(Add(1, 2, 4));                 
+        Console.WriteLine(Add(0.3, 0.2, 0.24534));       
+        Console.WriteLine(Add("text", "txet"));           
+        Console.WriteLine(Add('A', 'D', 'D'));             
     }
 
-    static dynamic Add(dynamic a, dynamic b)
+    static dynamic Add(params dynamic[] args)
     {
-        return a + b;
-    }
+        bool anyStringOrChar = false;
+        foreach (var arg in args)
+        {
+            if (arg is string || arg is char)
+            {
+                anyStringOrChar = true;
+                break;
+            }
+        }
 
-    static dynamic Add(dynamic a, dynamic b, dynamic c)
-    {
-        return a + b + c;
+        if (anyStringOrChar)
+        {
+            string result = "";
+            foreach (var arg in args)
+                result += arg.ToString();
+            return result;
+        }
+        else
+        {
+            dynamic sum = 0;
+            foreach (var arg in args)
+                sum += arg;
+            return sum;
+        }
     }
 }
